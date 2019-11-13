6c88c4bbcd1875e00b63c4cabd4436248833c884
mike <tmal0909@gmail.com>
2019-11-13 23:37:42 +0800

    [Web] Menu & 關於我們 js 控制完成

--- XinRevolution.Web/wwwroot/js/site.js
+++ XinRevolution.Web/wwwroot/js/site.js
@@ -3,4 +3,20 @@
     $(window).on('load', function () {
         $('#layout-animation-block').delay(1000).fadeOut("slow");
     });
+
+    // 事件 - Popup Show Btn 點選
+    $(document).on('click', '.js-popup-show-btn', function () {
+        var target = $(this).data('target');
+
+        if (target)
+            toggleBlock($(target), true);
+    });
+
+    // 事件 - Popup Close Btn 點選
+    $(document).on('click', '.js-popup-close-btn', function () {
+        var target = $(this).parents('.js-popup-block').first();
+
+        if (target)
+            toggleBlock($(target), false);
+    });
 });
--- XinRevolution.Web/wwwroot/js/util.js
+++ XinRevolution.Web/wwwroot/js/util.js
@@ -1,1 +1,7 @@
-﻿
+﻿// 開關區塊
+function toggleBlock($block, show) {
+    if (show)
+        $block.addClass('active');
+    else
+        $block.removeClass('active');
+}
