22003440642dca9d0af00d4ce0922ad17935d738
mike <tmal0909@gmail.com>
2019-11-18 21:50:10 +0800

    [Web] 修正註解

---
+++ XinRevolution.Web/Controllers/FireGenerationController.cs
@@ -0,0 +1,31 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace XinRevolution.Web.Controllers
+{
+    public class FireGenerationController : Controller
+    {
+        /// <summary>
+        /// 焰世代首頁
+        /// </summary>
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        /// <summary>
+        /// 角色介紹
+        /// </summary>
+        public IActionResult Character()
+        {
+            return View();
+        }
+
+        /// <summary>
+        /// 世界觀
+        /// </summary>
+        public IActionResult WorldView()
+        {
+            return View();
+        }
+    }
+}
